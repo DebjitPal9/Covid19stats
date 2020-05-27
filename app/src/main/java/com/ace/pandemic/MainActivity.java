bc0f7e251b139313b074e81d067e42edc9e67046
debupal99 <63508896+debupal99@users.noreply.github.com>
2020-05-27 19:38:45 +0530

    update1

--- app/src/main/java/com/ace/pandemic/MainActivity.java
+++ app/src/main/java/com/ace/pandemic/MainActivity.java
@@ -3,6 +3,13 @@ package com.ace.pandemic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View view) throws IOException, JSONException {
+        /*TextView tDeath1 =(TextView) findViewById(R.id.tDeath);
+        TextView nDeath1 =(TextView) findViewById(R.id.nDeath);
+        TextView tRecover1 =(TextView) findViewById(R.id.tRecover);
+        TextView tCases1 =(TextView) findViewById(R.id.tCases);
+        TextView nCases1 =(TextView) findViewById(R.id.nCases);*/
+
+        covidApi temp=new covidApi();
+
+        //tDeath1.setText(covidApi.totalDeaths);
+        Log.i("this",covidApi.totalDeaths);
+        /*nDeath1.setText(covidApi.newDeaths);
+        tRecover1.setText(covidApi.totalRecovered);
+        tCases1.setText(covidApi.totalCases);
+        nCases1.setText(covidApi.newCases);*/
     }
 }
--- app/src/main/java/com/ace/pandemic/covidApi.java
+++ app/src/main/java/com/ace/pandemic/covidApi.java
@@ -11,28 +11,25 @@ import okhttp3.Response;
 
 public class covidApi
 {
-    public String totalCases="",newCases="",totalRecovered="",totalDeaths="",newRecovered="",newDeaths="";
+    public static String totalCases="",newCases="",totalRecovered="",totalDeaths="",newDeaths="";
 
-    public covidApi() throws IOException, JSONException {
+       public covidApi() throws IOException, JSONException {
 
-        OkHttpClient client=new OkHttpClient();
+        OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url("https://disease.sh/v2/all").build();
         Response response = client.newCall(request).execute();
 
         String responseBody = "";
         responseBody = response.body().string();
 
-        JSONObject object=new JSONObject(responseBody);
+        JSONObject object = new JSONObject(responseBody);
         //now we fetch the data from the ob j
 
         totalCases = object.getString("cases");
         newCases = object.getString("todayCases");
         totalRecovered = object.getString("recovered");
         totalDeaths = object.getString("deaths");
-        newRecovered = object.getString("");
-        newDeaths = object.getString("deaths");
-
+        newDeaths = object.getString("todayDeaths");
     }
-    static int getDeaths(String)
 
 }
